16860c81ab758093619f1498d46102c6c9552cb5
Denis Matyukha <denmatuhawork@gmail.com>
2021-09-10 01:05:51 +0300

    final workable state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Header from './components/Header';
 import Layout from './components/Layout';
 import Footer from './components/Footer';
 
+// import bg1 from './assets/bg1.jpg'
+import bg2 from './assets/bg2.jpg'
+import bg3 from './assets/bg3.jpg'
+
 function App() {
   return (
     <div className="App">
@@ -15,7 +19,7 @@ function App() {
       <Layout 
         title='Layout_1__Title'
         descr='Layout_1__Description'
-        urlBg=''
+        urlBg={bg2}
       />
 
       <Layout 
@@ -27,7 +31,7 @@ function App() {
       <Layout 
         title='Layout_3__Title'
         descr='Layout_3__Description'
-        urlBg=''
+        urlBg={bg3}
       />
 
       <Footer />
@@ -37,13 +41,3 @@ function App() {
 }
 
 export default App;
-
-
-/*
-[]Первый и последний `Layout` должны принимать картинки, второй только цвет фона.
-
-[]`Layout` в котором должна быть картинка на фоне, вы должны использовать *inline* стиль `background` либо `backgroundImage`
-advise - Вы должны в компоненте App получить картинку, так как показано в уроке, и передать ее в соответсвующий props в компонент Layout
-
-[]Ты можешь выбрать одну из этих картинок, чтобы передать ее на фон для `Layout` или взять любую свою картинку, *as you wish...*
-*/
