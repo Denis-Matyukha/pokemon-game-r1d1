2de9869376fee4d10afb4d6378c8ad1e67a442ee
Denis Matyukha <denmatuhawork@gmail.com>
2021-09-09 21:01:15 +0300

    medium state of react components without imgs

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,27 @@
 import s from './style.module.css';
 
-const Layout = ({title, descr, urlBg, colorBg}) => {
+const Layout = ({ title, descr, urlBg, colorBg }) => {
     return (
-        <>
-            
-        </>
+        <section className={s.root}>
+            <div className={s.wrapper}>
+                <article>
+                    <div className={s.title}>
+                        <h3>
+                            {title}
+                        </h3>
+                        <span className={s.separator}></span>
+                    </div>
+
+                    {/* <div class="desc full">ИСПРАВИТЬ С ПРИМЕНЕНИЕМ БИБЛИОТЕКИ ДЛЯ НЕСКОЛЬКИХ ИМЕН КЛАССОВ В АТРИБУТЕ className */}
+                    <div class="desc full">
+                        <p>
+                            {descr}
+                        </p>
+                    </div>
+
+                </article>
+            </div>
+        </section>
     );
 };
 
