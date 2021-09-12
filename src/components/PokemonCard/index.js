8d4fb1d3f66b3a341590d8da3e624236d7107d51
Denis Matyukha <denmatuhawork@gmail.com>
2021-09-12 13:39:50 +0300

    pre-pre final not workable  draft state of a Code

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -1,35 +1,40 @@
 import s from './style.module.css';
 import cn from 'classnames';
 
-const PokemonCard = () => {
+import CardBackSide from './assets/card-back-side.jpg';
+
+const PokemonCard = ({...props}) => {
+
+    console.log(`###`, props);
+    
     return (
         <>
             <div  className={s.root}>
                 <div className={s.pokemonCard}>
                     <div className={s.cardFront}>
                         <div className={cn(s.wrap, s.front)}>
-                            <div className="pokemon <-- Type Pokemon -->">
+                            <div className={s.pokemon}>
                                 <div className={s.values}>
-                                    <div className="count top"><-- Count Value --></div>
-                                    <div className="count right"><-- Count Value --></div>
-                                    <div className="count bottom"><-- Count Value --></div>
-                                    <div className="count left"><-- Count Value --></div>
+                                    <div className={cn(s.count, s.top)}></div>
+                                    <div className={cn(s.count, s.right)}></div>
+                                    <div className={cn(s.count, s.bottom)}></div>
+                                    <div className={cn(s.count, s.left)}></div>
                                 </div>
                                 <div className={s.imgContainer}>
                                     <img src="<-- Pokemon Picture -->" alt="<-- Name Pokemon -->" />
                                 </div>
                                 <div className={s.info}>
-                                    <span className="number">#{< --ID Pokemon -->}</span>
-                                    <h3 className="name"><-- Name Pokemon --></h3>
-                                    <small className="type">Type: <span><-- Type Pokemon --></span></small>
+                                    <span className={s.number}>#{}</span>
+                                    <h3 className={s.name}></h3>
+                                    <small className={s.type}>Type: <span></span></small>
                                 </div>
                             </div>
                         </div>
                     </div>
 
                     <div className={s.cardBack}>
-                        <div className="wrap back">
-                            <img src="<-- Card Backed Picture -->" alt="Сard Backed" />
+                        <div className={cn(s.wrap, s.back)}>
+                            <img src={CardBackSide} alt="Сard Backed" />
                         </div>
                     </div>
 
