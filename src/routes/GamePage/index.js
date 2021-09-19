e5c76b2b8bd3f34bf3af6db7d3f5faaf5a8fc60e
Denis Matyukha <denmatuhawork@gmail.com>
2021-09-19 22:25:18 +0300

    Final workable state. The database is set to the initial primary condition.

--- src/routes/GamePage/index.js
+++ src/routes/GamePage/index.js
@@ -13,8 +13,6 @@ const GamePage = () => {
 
     useEffect(() => {
         database.ref('pokemons').once('value', (snapshot) => {
-            console.log(`  UseEffect сработал  и snapshot.val() принимает вид ↓`);
-            console.log(snapshot.val());
             setActivePokemons(snapshot.val());
         });
     }, []);
@@ -24,51 +22,20 @@ const GamePage = () => {
         setActivePokemons(prevState => {
 
             return Object.entries(prevState).reduce((acc, item) => {
-                // const key = [ ...item[0] ].join('');
                 const key = item[0];
                 const pokemon = { ...item[1] };
                 if (pokemon.id === pokeId) {
                     !pokemon.active ? pokemon.active = true : pokemon.active = false;
-                    // task2 ↓
                     database.ref('pokemons/' + key).set({
                         ...pokemon,
                         ...pokemon.active
                     });
-                    console.log(`TASK_2 - checkout the result`);
-                    // task 2 ↑
                 };
                 console.log(key);
-                // task2 ↓
-                // database.ref('pokemons/'+ objID).set({
-                // Один item покемона
-                // });
-                // task 2 ↑
-
                 acc[item[0]] = pokemon;
                 return acc;
             }, {});
         });
-
-        console.log(activePokemons);
-
-        // task 3 ↓
-        // const newKey = database.ref().child('pokemons').push().key;
-        // console.log(`task_3 newKey ↓`);
-        // console.log(newKey);
-        // console.log(`task_3 newKey ↑`);
-        /*
-        const newKey = database.ref().child('pokemons').push().key;
-        database.ref('pokemons/' + newKey).set(data);
-        */
-
-        // 3.1 сгенерить ключ и тело
-        // 3.2 запушить корректно в BD
-        // 3.3 обновить компонент из BD
-
-        // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/777.png
-        // Math.ceil(Math.random()*1000)
-        // task 3 ↑
-
     };
 
     const history = useHistory();
@@ -77,7 +44,6 @@ const GamePage = () => {
     const addRandomPokemon = () => {
         const getRandom = n => Math.ceil(Math.random() * n);
         const newKey = database.ref().child('pokemons').push().key;
-        // const newKey = `-tsk3_TSK3_`;
         const getNewPokemon = () => {
             return {
                 "abilities": [
@@ -112,61 +78,46 @@ const GamePage = () => {
 
         database.ref('pokemons/' + newKey).set(data);
 
-        // let newPokemonCard = {[`${newKey}`]: { ...newPokemon }};
-        // console.log(`TSK_3 ↓`);
-        // console.log(newPokemonCard);
-        // console.log({newPokemonCard});
-        // console.log(`TSK_3 ↑`);
-        // console.log(`!_TSK_3_!   ↓`);
-        // console.log({...{[`${newKey}`]: { ...newPokemon }}});
-        // console.log({newPokemonCard});
-        // console.log(`!_TSK_3_!   ↑`);
-        // return {...{[`${newKey}`]: { ...newPokemon }}};
-
         database.ref('pokemons').once('value', (snapshot) => {
-            console.log(`  UseEffect сработал  и snapshot.val() принимает вид ↓`);
-            console.log(snapshot.val());
             setActivePokemons(snapshot.val());
         });
-        
     };
 
-return (
-    <>
-        <button className={s.addBtn} onClick={addRandomPokemon}>
-            click to add a Random pokemon ↓
-        </button>
-
-        <div id="test" className={s.flex}>
-
-
-            {
-                Object.entries(activePokemons).map(([key, { name, id, img, type, values, active }]) => {
-
-                    return (
-                        <PokemonCard
-                            key={key}
-                            name={name}
-                            id={id}
-                            img={img}
-                            type={type}
-                            values={values}
-                            active={active}
-                            idTransfer={onClickHandler}
-                        />)
-                })
-            }
-
-        </div>
-
-        <div className={s.wrap}>
-            this is Game Page component.
-            <button className={s.gameBackBtn} onClick={gameBtnHandler}>
-                ← Back to Home Page
+    return (
+        <>
+            <button className={s.addBtn} onClick={addRandomPokemon}>
+                Click to add a Random pokemon ↓
             </button>
-        </div>
-    </>
-);
+
+            <div id="test" className={s.flex}>
+
+                {
+                    Object.entries(activePokemons).map(([key, { name, id, img, type, values, active }]) => {
+
+                        return (
+                            <PokemonCard
+                                key={key}
+                                name={name}
+                                id={id}
+                                img={img}
+                                type={type}
+                                values={values}
+                                active={active}
+                                idTransfer={onClickHandler}
+                            />)
+                    })
+                }
+
+            </div>
+
+            <div className={s.wrap}>
+                this is Game Page component.
+                <button className={s.gameBackBtn} onClick={gameBtnHandler}>
+                    ← Back to Home Page
+                </button>
+            </div>
+        </>
+    );
 };
 
 export default GamePage;
