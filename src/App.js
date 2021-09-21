c8cebea61dffb32442a22bf78c8e984fe02501f7
Denis Matyukha <denmatuhawork@gmail.com>
2021-09-21 18:29:09 +0300

    workable draft state with ContextAPI exapmle used in About_Page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useRouteMatch, Route, Switch, Redirect } from "react-router-dom";
 
 import cn from 'classnames';
@@ -8,37 +9,46 @@ import HomePage from "./routes/HomePage";
 import AboutPage from "./routes/AboutPage";
 import ContactPage from "./routes/ContactPage";
 import NotFoundPage from "./routes/NotFound";
-
+// import { TestContext } from "./context/testContext";
 import s from './style.module.css';
 
+
+export const TestContext = React.createContext(null);
 
 const App = () => {
 
   const match = useRouteMatch('/');
   return (
-    <Switch>
-      <Route path="/404" component={NotFoundPage} />
-      <Route>
-        <>
-          <MenuHeader bgActive={!match.isExact} />
-          <div className={cn(s.wrap, {
-            [s.isHomePage]: match.isExact
-          })}>
-            <Switch>
-              <Route path="/" exact component={HomePage} />
-              <Route path="/home" component={HomePage} />
-              <Route path="/game" component={GamePage} />
-              <Route path="/about" component={AboutPage} />
-              <Route path="/contact" component={ContactPage} />
-              <Route render={() => (
-                <Redirect to="/404" />
-              )} />
-            </Switch>
-          </div>
-          <Footer />
-        </>
-      </Route>
-    </Switch>
+    <>
+      <TestContext.Provider value={{
+        theme: 'light'
+      }}>
+
+        <Switch>
+          <Route path="/404" component={NotFoundPage} />
+          <Route>
+            <>
+              <MenuHeader bgActive={!match.isExact} />
+              <div className={cn(s.wrap, {
+                [s.isHomePage]: match.isExact
+              })}>
+                <Switch>
+                  <Route path="/" exact component={HomePage} />
+                  <Route path="/home" component={HomePage} />
+                  <Route path="/game" component={GamePage} />
+                  <Route path="/about" component={AboutPage} />
+                  <Route path="/contact" component={ContactPage} />
+                  <Route render={() => (
+                    <Redirect to="/404" />
+                  )} />
+                </Switch>
+              </div>
+              <Footer />
+            </>
+          </Route>
+        </Switch>
+      </TestContext.Provider>
+    </>
   )
 };
 
--- src/routes/AboutPage/index.js
+++ src/routes/AboutPage/index.js
@@ -1,11 +1,16 @@
 import { useHistory } from 'react-router-dom';
+import { useContext } from 'react';
 import s from './style.module.css';
+import { TestContext } from '../../App';
 
 const AboutPage = ({ onChangePage }) => {
     const history = useHistory();
     const onClickHandler = () => {
         history.push('/');
     }
+
+    const themeContext = useContext(TestContext);
+    console.log(`themeContext → `,themeContext);
 
     return (
         <>
